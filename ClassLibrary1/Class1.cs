81296282c5e27a55849e9fe6dd5653c3fcd42854
Лещенко Даниил Евгеньевич <leschenko.de1634@okeit.edu>
2023-04-24 10:29:11 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public static class Class1
+    {
+        public static double Plus()
+        {
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            return A + B;
+        }
+        public static double Minus()
+        {
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            return A - B;
+        }
+        public static double Multiply()
+        {
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            return A * B;
+        }
+        public static double Divide()
+        {
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            return A / B;
+        }
+    }
+}
---
+++ Math/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLib;
+
+namespace Calculator
+{
+    class Plus
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathLib.Class1.Plus());//Сложение
+            Console.ReadLine();
+            Console.WriteLine(MathLib.Class1.Minus());//Вычитание
+            Console.ReadLine();
+            Console.WriteLine(MathLib.Class1.Multiply());//Умножение
+            Console.ReadLine();
+            Console.WriteLine(MathLib.Class1.Divide());//Деление
+            Console.ReadLine();
+        }
+    }
+}
